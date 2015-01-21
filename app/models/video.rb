dd66a8b1a6b7d5040f3d7888d87d809fa14c4279
eddie <eddie@visionbundles.com>
2015-01-21 17:33:22 +0800

    Remove video default_scope

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,5 @@
 class Video < ActiveRecord::Base
-
-  default_scope { order(id: :desc) }
-
+  self.per_page = 10
   has_many :tags, through: :video_tagships
   has_many :girls, through: :video_girlships
   has_many :video_girlships
