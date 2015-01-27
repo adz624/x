bed3a41b8180845f147ebd6fae9a5b18aad88b6e
eddie <eddie@visionbundles.com>
2015-01-28 00:13:50 +0800

    Update player height, width, image

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -1,6 +1,11 @@
 module VideoHelper
   def render_player(video, opt={})
-    options = opt.merge(flashplayer: '/player.swf', provider: '', width: '100%', file: video.rtmp_fullpath)
+    options = opt.merge(flashplayer: '/player.swf',
+                        provider: '',
+                        width: '100%',
+                        height: '800',
+                        file: video.rtmp_fullpath,
+                        image: video.image_path)
     jwplayer(options)
   end
 end
